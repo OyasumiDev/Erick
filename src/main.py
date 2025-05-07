a85f3372a3af0cd7d7875263b8addd5e79d92ad5
NEON <alejandroerick144@gmail.com>
2025-05-07 01:35:21 -0600

    Commit del programa actual

--- src/main.py
+++ src/main.py
@@ -1,2 +1,22 @@
+import mysql.connector
 from models.auto_model import AutoModel
+
+AutoModel.add("Toyota", "Corolla", 2020, "Rojo", 15000.0)
+
+def ver_autos():
+    conexion = mysql.connector.connect(
+        host="localhost",
+        user="tu_usuario",
+        password="tu_contraseña",
+        database="BASE_AUTOS"
+    )
+    cursor = conexion.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM autos")
+    autos = cursor.fetchall()
+
+    for auto in autos:
+        print(auto)
+
+    conexion.close()
+
 
